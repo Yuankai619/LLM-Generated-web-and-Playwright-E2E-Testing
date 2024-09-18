3f46f9f46685cd03b3c2847a8b8e0b6c1477109b
owen0806 <owen080614@gmail.com>
2024-09-18 21:10:01 +0800

    Update playwright.config.ts

--- test2/playwright.config.ts
+++ test2/playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
     baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
